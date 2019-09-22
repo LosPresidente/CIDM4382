f01815a285babb68165d93d172c784655adf9347
LosPresidente <ibilek1@buffs.wtamu.edu>
2019-09-22 17:33:48 -0500

    done

--- loc8r/app_server/controllers/others.js
+++ loc8r/app_server/controllers/others.js
@@ -1,4 +1,8 @@
 //GET about page
 const about = (req, res) => {res.render('generic-text', {title: 'About'})};
-module.exports = {about};
+const pizza = (req, res) => {res.render('pizza', {title: 'Pizza Places'})};
+module.exports = {
+    about,
+    pizza
+};
 
--- loc8r/app_server/routes/index.js
+++ loc8r/app_server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+//this line says ctrlLocations = controllers/locations.js
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 //const ctrlMain = require('../controllers/others');
@@ -9,11 +10,13 @@ const ctrlOthers = require('../controllers/others');
 //location pages
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
+router.get('/pizzadetails', ctrlLocations.pizzaInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
 //other pages
 
 router.get('/about', ctrlOthers.about);
+router.get('/pizza', ctrlOthers.pizza);
 
 //router.get('/', ctrlMain.index);
 
